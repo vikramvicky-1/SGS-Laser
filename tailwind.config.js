2adec0c056713d579af1b06c8dba29f5301b08d0
vikram <123199376+vikramvicky-1@users.noreply.github.com>
2026-01-30 16:12:34 +0530

    color fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        background: '#FFF8F0',
-        primary: '#531CB3',
-        secondary: '#C2410C',
-        text: '#292929',
-        accent: '#6936C0',
-        lightText: '#4F4F4F',
-        orangeAccent: '#FF7A2F',
+        background: "#FFF8F0",
+        primary: "#531CB3",
+        secondary: "#e66a22",
+        text: "#292929",
+        accent: "#6936C0",
+        lightText: "#4F4F4F",
+        orangeAccent: "#FF7A2F",
       },
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
+        inter: ["Inter", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
-
+};
